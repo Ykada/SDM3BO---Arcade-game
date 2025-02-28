89493f7ed6394ba671e095318658e6e73fa64713
Yk_ <bo.bolweg@gmail.com>
2025-02-28 09:04:26 +0100

    Map 3, Ui Update

---
+++ Assets/LoadingMapScript.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;  // If you're working with UI elements like buttons, text, etc.
+
+public class DisableUIAfterRandomTime : MonoBehaviour
+{
+    public GameObject uiElement;
+    public float minTime = 1f;
+    public float maxTime = 5f;
+
+    private float randomTime;
+    private float timer;
+
+    void Start()
+    {
+        randomTime = Random.Range(minTime, maxTime);
+        timer = 0f;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= randomTime)
+        {
+            DisableUI();
+        }
+    }
+
+    // Disable the UI element
+    void DisableUI()
+    {
+        if (uiElement != null)
+        {
+            uiElement.SetActive(false);
+        }
+    }
+}
